6ee5dea7015a10b5de119f117659b803c64ce870
Reed <rtuckerbass@gmail.com>
2020-10-21 13:12:30 -0400

    improved scrape

--- generate_rhyme.py
+++ generate_rhyme.py
@@ -2,8 +2,6 @@
 
 ### this program should:
 ###     *take a list of words
-###     *query the web to see if it's a verb
-###     *if it is a verb, record the IPA in a dict where the word is the key
 ###     *make a list of possible IPA vowels
 ###     *find the index of the first IPA vowel
 ###     *cut off everthing before the first vowel
@@ -24,14 +22,7 @@ def main():
 def query_web_dict(site, word):
     page = requests.get(site + word)
     soup = BeautifulSoup(page.content, 'html.parser')
-    part_of_speech = pos_check(soup)
-    print(part_of_speech)
 
-def pos_check(soup):
-    raw_pos_text = soup.find(class_="luna-pos").text
-    pos = raw_pos_text[:4]
-    if pos == 'verb':
-        return (pos, raw_pos_text[5:])
 
 if __name__ == "__main__":
     main()
