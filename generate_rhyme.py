020a3a173832353eb5b3df8fdc7d753ced07ce8c
Reed <rtuckerbass@gmail.com>
2020-10-21 11:59:55 -0400

    added outline

---
+++ generate_rhyme.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+### this program should:
+###     *take a list of words
+###     *query the web to see if it's a verb
+###     *if it is a verb, record the IPA in a dict where the word is the key
+###     *make a list of possible IPA vowels
+###     *find the index of the first IPA vowel
+###     *cut off everthing before the first vowel
+###
+### this will make only strict rhymes. future functionality could be to have a switch to allow loose rhymes as well
+###
+###
+
